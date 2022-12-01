297746c267c14404d2e9d1355efb873b9743c8c4
Vedant Thakre <44766005+vedant-github@users.noreply.github.com>
2022-12-01 16:08:53 +0530

    added images

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,13 +1,17 @@
 import { styled } from "@material-ui/styles";
 import { Card, Hidden, Typography } from "@mui/material";
-import { borderRadius, Box, shadows } from "@mui/system";
+import { borderRadius, Box, fontSize, shadows } from "@mui/system";
 import React from "react";
 
 import gallery_1 from '../assets/gallery_1.webp'
 import gallery_2 from '../assets/gallery_2.webp'
 import gallery_3 from '../assets/gallery_3.webp'
 import gallery_4 from '../assets/gallery_4.webp'
-import { ArrowBackIosOutlined, NoEncryption } from "@material-ui/icons";
+import dimension_1_1 from '../assets/dimension_1_1.webp'
+import box from '../assets/box.png'
+import install from '../assets/install.png'
+
+import { ArrowBackIosOutlined, } from "@material-ui/icons";
 
 
 export default function Page1() {
@@ -72,21 +76,88 @@ export default function Page1() {
           >Add to Cart</button>
         </div>
 
-        <div styles={{width:450,
-           height:60,
-           marginLeft:50,
-           borderRadius:20,
+        <div style={{width:450,
+           height:200,
+           marginLeft:20,
+           marginTop:-220,
+           zIndex:10,
+           boxShadow:'10 10 red',
            padding:50,           
            background:"#ffffff",
-           color:'#07455f',
-           border: '2px solid red'}}>
-          <h4>About the Product</h4>
-          <h5>Lorem ipsum dolor sit amet consectetur 
-            adipisicing elit. Dicta veritatis non ab voluptates 
-            temporibus adipisci blanditiis doloremque, sapiente nul
-            la quos minus enim quasi commodi assumenda deserunt molestias
-             tempore. Sapiente fuga eligendi blanditiis!</h5>
+           color:'#07455f'}}>
+            <p style={{fontSize:30}}>About the Product</p>
+            <p style={{fontSize:16, color:'grey'}}>This plush and inviting L-Shaped Sofa has 
+              luxurious soft upholstery which not only provides
+              comfortable seating for long hours of lounging but
+              also makes it a statement piece for your room</p>
         </div>
+
+        <div style={{textAlign:'center',
+         marginTop:40,
+         color:'#07455f',
+         fontSize:40}}> Details
+        </div>
+
+        <div style={{width:450,
+           height:200,
+           marginLeft:20,
+           marginTop:60,
+           zIndex:10,
+           boxShadow:'10 10 red',
+           padding:50,           
+           background:"#ffffff",
+           color:'#07455f'}}>
+            <p style={{fontSize:40}}>Specifications</p>
+            <pre style={{fontSize:16, color:'grey'}}>
+              Size              L-Shape<br/>
+              Color             Blue Opal<br/>
+              Dimensions        255cm x 178cm x 86.5cm
+              </pre>
+            <img src={dimension_1_1} style={{width:500,
+            marginLeft:-100,
+            }}></img>  
+        </div>
+
+
+        <div style={{marginTop:400,
+        alignItems:'center',
+        textAlign:'center',
+        width:320, }}>
+        <img style={{width:80,
+            marginBottom:50}} src={box} alt="logo" />
+        <div>
+          <b>Free Shipping</b><br/>
+          We deliver the product at your doorstep, at no extra charge.
+        </div>   </div>
+
+        <div style={{marginTop:-200,
+        marginLeft: '34%',
+        alignItems:'center',
+        textAlign:'center',
+        width:320, }}>
+        <img style={{width:80,
+            marginBottom:50}} src={install} alt="logo" />
+        <div>
+          <b>Hassle-free Installation</b><br/>
+          We assemble the product and clear away the packaging
+          , leaving you to enjoy the product.
+        </div>   </div>
+
+        
+        <div style={{marginTop:-200,
+        marginLeft: '68%',
+        alignItems:'center',
+        textAlign:'center',
+        width:320, }}>
+        <img style={{width:80,
+            marginBottom:50}} src={install} alt="logo" />
+        <div>
+          <b>Hassle-free Installation</b><br/>
+          We assemble the product and clear away the packaging
+          , leaving you to enjoy the product.
+        </div>   </div>
+
+
 
        </div>
 
